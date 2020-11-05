24a2632bfc3d4ca6a95f12f4072bd7d71b6cece3
Billy J. Beltran <billy@brightraven.eu>
2020-11-05 21:12:31 +0000

    Add elisp/cyrillic.el, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # emacs-ocs
 EMACS elisp for Old Church Slavonic
+
+Contains a new input method `cyrillic-ocs` in cyrillic.el
+that permits using Latin transliteration to write OCS.
---
+++ elisp/cyrillic.el
@@ -0,0 +1,52 @@
+(require 'quail)
+
+;; This mapping uses Unicode
+
+
+(quail-define-package "cyrillic-ocs" "Old Church Slavonic" "ocsc" t
+
+		      "Intuitive keyboard layout for OCS. "
+		      nil t t t t nil nil nil nil nil t)
+(quail-define-rules
+ ("b" ?б) ("v" ?в)
+ ("g" ?г) ("d" ?д)
+
+ ("p" ?п) ("t" ?т)
+ ("k" ?к)
+ 
+ ("r" ?р) ("l" ?л)
+ ("n" ?н) ("m" ?м)
+
+ ("s" ?с)
+ ("z" ?ꙁ) ("/z" ?з)
+ ("zh" ?ж)
+ ("dz" ?ꙃ) ("/dz" ?ѕ)
+ ("sht" ?щ) ("sh" ?ш)
+ ("x" ?х)
+ ("ch" ?ч)  ("c" ?ц)
+ ; greek letters
+ ("F" ?ф)
+ ("KS" ?ѯ)
+ ("PS" ?ѱ) ("Y" ?ѵ)
+ ("TH" ?ѳ) ("Q" ?ҁ)
+ ; vowels
+ ("w" ?ѡ)
+ ("i" ?и)  ("y" ?ꙑ)
+ ("e" ?є) ("o" ?о)
+ ("/e" ?ѣ) ("/i" ?і)
+ ("a" ?а)
+ ("u" ?о?у) ("u/o" ?ꙋ)
+ ; yus
+ ("eN" ?Ѧ) ("oN" ?ѫ)
+ ; yers
+  ("U" ?ъ) ("'I" ?ь)
+ ; iotated vowels
+ ("ye" ?ѥ) ("ya" ?ꙗ) ("yu" ?ю)
+ ("yeN" ?ѩ) ("yoN" ?ѭ)
+
+ ;diacritics
+ ("-" ?҃)
+ 
+ )
+
+ 
